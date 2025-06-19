6b9b181b4eff28b87f53011ab4d22f32317fa9df
(Heeyong Ryu) <(rhyong1203@gmail.com)>
2025-06-19 14:47:34 +0900

    Revert: 이전 충돌 임시 수정을 원래대로 되돌림
    This reverts commit 4d44e2a47f317f0c17ccad06baa4b0a118431f5e, reversing
    changes made to 1982eb973ff314886afa01bb955a7b999ac25a81.

--- src/main/java/com/sinse/wms/common/view/Main.java
+++ src/main/java/com/sinse/wms/common/view/Main.java
@@ -3,13 +3,6 @@ package com.sinse.wms.common.view;
 import java.awt.BorderLayout;
 import java.awt.CardLayout;
 import java.awt.Color;
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-import java.awt.Dimension;
-=======
->>>>>>> efcca38 (현재까지 작업 내용 저장)
->>>>>>> 8fcb24f (현재까지 작업 내용 저장)
 
 import javax.swing.JFrame;
 import javax.swing.JOptionPane;
@@ -17,29 +10,11 @@ import javax.swing.JPanel;
 
 import com.sinse.wms.common.Config;
 import com.sinse.wms.common.util.SideMenuHelper;
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-import com.sinse.wms.common.view.content.ExampleContentPage;
->>>>>>> 8fcb24f (현재까지 작업 내용 저장)
 import com.sinse.wms.common.view.content.ContentHeader;
 import com.sinse.wms.common.view.sidemenu.BaseSideMenu.SideMenuClickListener;
 import com.sinse.wms.common.view.sidemenu.Menu;
 import com.sinse.wms.common.view.sidemenu.SideBar;
 import com.sinse.wms.common.view.sidemenu.SideMenuGroup;
-<<<<<<< HEAD
-=======
-import com.sinse.wms.common.view.sidemenu.BaseSideMenu.SideMenuClickListener;
-=======
-import com.sinse.wms.common.view.content.ContentHeader;
-import com.sinse.wms.common.view.content.ExampleContentPage;
-import com.sinse.wms.common.view.content.InboundStatusPage;
-import com.sinse.wms.common.view.sidemenu.BaseSideMenu.SideMenuClickListener;
-import com.sinse.wms.common.view.sidemenu.Menu;
-import com.sinse.wms.common.view.sidemenu.SideBar;
-import com.sinse.wms.common.view.sidemenu.SideMenuGroup;
->>>>>>> efcca38 (현재까지 작업 내용 저장)
->>>>>>> 8fcb24f (현재까지 작업 내용 저장)
 import com.sinse.wms.common.view.toolbar.ToolBar;
 import com.sinse.wms.common.view.toolbar.ToolBar.ToolBarListener;
 import com.sinse.wms.inbound.view.InboundInspectionPage;
@@ -58,75 +33,70 @@ import com.sinse.wms.outbound.view.OutboundStatusPage;
 import com.sinse.wms.product.model.Member;
 import com.sinse.wms.report.view.ReportPage;
 import com.sinse.wms.statistic.view.StatisticPage;
-import com.sinse.wms.inbound.view.InboundInspectionPage;
-import com.sinse.wms.inbound.view.InboundRequestPage;
-import com.sinse.wms.inbound.view.InboundStatusPage;
-import com.sinse.wms.inventory.view.InventoryStatusPage;
-import com.sinse.wms.outbound.view.OutboundInspectionPage;
-import com.sinse.wms.outbound.view.OutboundRequestPage;
-import com.sinse.wms.outbound.view.OutboundStatusPage;
 
 public class Main extends JFrame implements SideMenuClickListener, ToolBarListener {
-	private ToolBar toolbar;
-	private SideBar sidebar;
-	private JPanel centerWrapper;
-	private JPanel bodyWrapper;
-	private ContentHeader header;
-	private JPanel bodyContent;
-	private CardLayout cardLayout;
-	private SideMenuGroup[] sideMenuGroups;
-	Member m; //로그인한 사용자 정보 받아옴
+    private ToolBar toolbar;
+    private SideBar sidebar;
+    private JPanel centerWrapper;
+    private JPanel bodyWrapper;
+    private ContentHeader header;
+    private JPanel bodyContent;
+    private CardLayout cardLayout;
+    private SideMenuGroup[] sideMenuGroups;
+    Member m; // 로그인한 사용자 정보 받아옴
 
-	public Main(Member m) {
-		this.m = m;
-		setSize(Config.MAIN_WINDOW_WIDTH, Config.MAIN_WINDOW_HEIGHT);
-		initToolbar();
-		initSideBar();
-		initHeader();
-		initContents();
+    public Main(Member m) {
+        this.m = m;
+        setSize(Config.MAIN_WINDOW_WIDTH, Config.MAIN_WINDOW_HEIGHT);
+        initToolbar();
+        initSideBar();
+        initHeader();
+        initContents();
 
-		this.bodyWrapper = new JPanel(new BorderLayout());
-		this.bodyWrapper.add(header, BorderLayout.NORTH);
-		this.bodyWrapper.add(bodyContent, BorderLayout.CENTER);
-		this.centerWrapper = new JPanel(new BorderLayout());
-		this.centerWrapper.add(sidebar, BorderLayout.WEST);
-		this.centerWrapper.add(bodyWrapper, BorderLayout.CENTER);
+        this.bodyWrapper = new JPanel(new BorderLayout());
+        this.bodyWrapper.add(header, BorderLayout.NORTH);
+        this.bodyWrapper.add(bodyContent, BorderLayout.CENTER);
+        this.centerWrapper = new JPanel(new BorderLayout());
+        this.centerWrapper.add(sidebar, BorderLayout.WEST);
+        this.centerWrapper.add(bodyWrapper, BorderLayout.CENTER);
 
-		add(toolbar, BorderLayout.WEST);
-		add(centerWrapper, BorderLayout.CENTER);
+        add(toolbar, BorderLayout.WEST);
+        add(centerWrapper, BorderLayout.CENTER);
 
-		toolbar.setToolBarListener(this);
-		
-		setVisible(true);
-		pack();
-	}
+        toolbar.setToolBarListener(this);
 
-	/**
-	 * 툴바 초기화 함수
-	 */
-	private void initToolbar() {
-		this.toolbar = new ToolBar();
-		this.toolbar.setToolBarListener(this);
-	}
+        setVisible(true);
+        pack();
+    }
 
-	/**
-	 * 사이드메뉴바 초기화 함수
-	 */
-	private void initSideBar() {
-		this.sideMenuGroups = new SideMenuGroup[] { SideMenuHelper.createSideMenu(Menu.IN_BOUND_STATUS, this),
-				SideMenuHelper.createSideMenu(Menu.OUT_BOUND_STATUS, this),
-				SideMenuHelper.createSideMenu(Menu.INVENTORY_STATUS, this),
-				SideMenuHelper.createSideMenu(Menu.STATISTICS, this),
-				SideMenuHelper.createSideMenu(Menu.USER_MANAGEMENT, this) };
-		this.sidebar = new SideBar("사용자 페이지", sideMenuGroups);
-	}
+    /**
+     * 툴바 초기화 함수
+     */
+    private void initToolbar() {
+        this.toolbar = new ToolBar();
+        this.toolbar.setToolBarListener(this);
+    }
 
-	/**
-	 * 헤더 초기화 함수
-	 */
-	private void initHeader() {
-		this.header = new ContentHeader("마케팅 1팀", "김범수");
-	}
+    /**
+     * 사이드메뉴바 초기화 함수
+     */
+    private void initSideBar() {
+        this.sideMenuGroups = new SideMenuGroup[] {
+            SideMenuHelper.createSideMenu(Menu.IN_BOUND_STATUS, this),
+            SideMenuHelper.createSideMenu(Menu.OUT_BOUND_STATUS, this),
+            SideMenuHelper.createSideMenu(Menu.INVENTORY_STATUS, this),
+            SideMenuHelper.createSideMenu(Menu.STATISTICS, this),
+            SideMenuHelper.createSideMenu(Menu.USER_MANAGEMENT, this)
+        };
+        this.sidebar = new SideBar("사용자 페이지", sideMenuGroups);
+    }
+
+    /**
+     * 헤더 초기화 함수
+     */
+    private void initHeader() {
+        this.header = new ContentHeader("마케팅 1팀", "김범수");
+    }
 
 	/**
 	 * 메뉴에 따라서 유저에게 보여지는 콘텐츠를 초기화 하는 함수
@@ -153,37 +123,38 @@ public class Main extends JFrame implements SideMenuClickListener, ToolBarListen
 		this.bodyContent.add(new SettingMenu(), Menu.SETTING.name());	//환경설정 페이지
 	}
 
-	/**
-	 * 사이드 메뉴 클릭시 해당 인터페이스 함수로
-	 * 
-	 * @param Menu 가 파라미터로 호출됨
-	 * @see SideMenuClickListener
-	 */
-	@Override
-	public void onClick(Menu menu) {
-		onChangeMenu(menu);
-	}
+    /**
+     * 사이드 메뉴 클릭시 해당 인터페이스 함수로
+     *
+     * @param menu 가 파라미터로 호출됨
+     * @see SideMenuClickListener
+     */
+    @Override
+    public void onClick(Menu menu) {
+        onChangeMenu(menu);
+    }
 
-	/**
-	 * 메뉴가 변경되었을때 핸들링 함수를 호출하는 함수 사이드메뉴를 핸들링 하는 함수와 컨텐츠를 핸들링하는 함수를 호출한다.
-	 * 
-	 * @param menu
-	 */
-	private void onChangeMenu(Menu menu) {
-		hangleSideMenu(menu);
-		handleContent(menu);
-	}
+    /**
+     * 메뉴가 변경되었을때 핸들링 함수를 호출하는 함수
+     * 사이드메뉴를 핸들링 하는 함수와 컨텐츠를 핸들링하는 함수를 호출한다.
+     *
+     * @param menu
+     */
+    private void onChangeMenu(Menu menu) {
+        hangleSideMenu(menu);
+        handleContent(menu);
+    }
 
-	/**
-	 * 사이드메뉴의 레이아웃을 업데이트 하는 함수
-	 * 
-	 * @param menu
-	 */
-	private void hangleSideMenu(Menu menu) {
-		for (int i = 0; i < sideMenuGroups.length; i++) {
-			sideMenuGroups[i].onChangeMenu(menu);
-		}
-	}
+    /**
+     * 사이드메뉴의 레이아웃을 업데이트 하는 함수
+     *
+     * @param menu
+     */
+    private void hangleSideMenu(Menu menu) {
+        for (int i = 0; i < sideMenuGroups.length; i++) {
+            sideMenuGroups[i].onChangeMenu(menu);
+        }
+    }
 
 	/**
 	 * 컨텐츠를 교체하는 하는 함수
@@ -206,16 +177,16 @@ public class Main extends JFrame implements SideMenuClickListener, ToolBarListen
         }
     }
 
-	@Override
-	public void onClickMyPage() {
-		System.out.println("마이페이지클릭"); //클릭 이벤트 수신
-		this.cardLayout.show(this.bodyContent, Menu.MY_PAGE.name());
-	}
+    @Override
+    public void onClickMyPage() {
+        System.out.println("마이페이지클릭"); // 클릭 이벤트 수신
+        this.cardLayout.show(this.bodyContent, Menu.MY_PAGE.name());
+    }
 
-	@Override
-	public void onClickMessageBox() {
-		// TODO("툴바 메시지 아이콘 클릭시 구현");
-	}
+    @Override
+    public void onClickMessageBox() {
+        // TODO("툴바 메시지 아이콘 클릭시 구현");
+    }
 
 	@Override
 	public void onClickSearch() {
